3bee0d42a8a0cbeeea94cf524015e33d904c4e5d
GBruening <garrickbruening@gmail.com>
2020-07-16 16:03:39 -0600

    Changed to only calculate sum of torques across a range of masses and movement durations from minimum jerk

--- looper2.m
+++ looper2.m
@@ -9,28 +9,10 @@ function [out] = looper2( Data, forearm , upperarm, vars)
     %% Inverse dyanmics to joint torques
     [ shoulder , elbow , theta , eff_mass] = ...
         Calc_kine( Data, forearm, upperarm,vars.time_inc,vars.masses);
-
-    %% Minimization of cost function by time step
-    [ muscles , act , u , est , tnew] =...
-        compute_min_cost(shoulder,elbow,theta,...
-        upperarm,forearm,vars);
-        
-    %% Calc Energy Rates
-    muscle_nums = {'an','bs','br','da','dp','pc','bb','tb'};
-    for k=1:8
-        [~,energy.(muscle_nums{k})] = ...
-            umberger2(muscles.(muscle_nums{k}) , act.(muscle_nums{k}) , u.(muscle_nums{k}) );
-    end
     
     out.shoulder = shoulder;
     out.elbow    = elbow;
     out.theta    = theta;
-    out.muscles  = muscles;
-    out.act      = act;
-    out.u        = u;
-    out.est      = est;
-    out.tnew     = tnew;
-    out.energy   = energy;
     out.eff_mass = eff_mass;
 end
 
